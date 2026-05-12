23136f1a254bcdfba1a1bc65756f2cbcb0f6e8a5
ashley104 <dangthaomy13062013@gmail.com>
2026-05-12 21:53:07 +0000

    fix: prisma user auth

---
+++ packages/db/prisma/migrations/20260512215007_delete_post/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Like` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- DropTable
+DROP TABLE "Like";
+
+-- DropTable
+DROP TABLE "Post";
--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -17,38 +17,4 @@ export async function seed() {
       },
     });
   }
-  
-  // TODO: Uncomment below once you set up Prisma and loaded data to your database
-
-  // console.log("🌱 Seeding data");
-  // await client.like.deleteMany();
-  // await client.post.deleteMany();
-  // for (const post of posts) {
-  //   await client.post.create({
-  //     data: {
-  //       title: post.title,
-  //       content: post.content,
-  //       category: post.category,
-  //       description: post.description,
-  //       imageUrl: post.imageUrl,
-  //       tags: post.tags
-  //         .split(",")
-  //         .map((p) => p.trim())
-  //         .join(","),
-  //       urlId: post.urlId,
-  //       active: post.active,
-  //       date: post.date,
-  //       id: post.id,
-  //       views: post.views,
-  //     },
-  //   });
-  //   for (let i = 0; i < post.likes; i++) {
-  //     await client.like.create({
-  //       data: {
-  //         postId: post.id,
-  //         userIP: `192.168.100.${i}`,
-  //       },
-  //     });
-  //   }
-  // }
 }
